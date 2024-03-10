11f5dbc2f9c381a41b41b6f64ea510d2eff4dd3b
Amit Manna <amitmanna@AmitM-KrsnaR-Macbook-Air.local>
2024-03-10 11:18:03 +0530

    routes created

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
+import Login from './components/login/Login.component';
+import Signup from './components/signup/Signup.component';
+import Logout from './components/logout/Logout.component';
+import Dashboard from './components/dashboard/Dashboard.component';
+
 
 function App() {
 	return (
-		<>
-		</>
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={ <Navigate to={'user/login'} />} />
+				<Route path='user/login' element={ <Login /> } />
+				<Route path='user/signup' element={ <Signup /> } />
+				<Route path='user/logout' element={ <Logout /> } />
+				<Route path='user/dashboard' element={ <Dashboard /> } />
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
